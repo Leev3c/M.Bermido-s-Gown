f028739574e7bec3467089e20e6e12500dc9f6ea
Leev3c <82589504+Leev3c@users.noreply.github.com>
2021-04-27 20:57:51 +0800

    aboutus tab for the index.php

---
+++ M.Bermidos Gown/aboutus.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About us</title>
+	<link rel="stylesheet" href="css/aboutus.css" />
+</head>
+<body>
+	<div class="container">
+		<header>
+			<div class="logo">
+			
+			</div>
+			<div class="main-nav">
+				<ul>
+					<li>
+						<a href="index.html">Home</a>
+					</li>
+					<li>
+						<a href="products.html">Products</a>
+					</li>
+					<li>
+						<a href="services.html">Services</a>
+					</li>
+					<li>
+						<a href="aboutus.html">About us</a>
+					</li>
+					<li>
+						<a href="sign-in.html">Sign-in</a>
+					</li>
+				</ul>
+			</div>
+		
+	
+		</header>
+
+		<content>
+			<div class="content">
+					<!-- <img src="img/logo.jpg" width="650" height="300"> -->
+					<div class="about-us">
+						<h1><a href="https://www.facebook.com/mbermido/"><em>M.Bermidos</em> <br> <br></a> We do Wedding Entourage Formal Wear, Wedding Gowns, and Dresses. This includes Evening, Prom, Pageant, Debut, and Casual.</h1>
+					</div>
+			</div>
+			
+			<div class="banner">
+			
+			</div>
+		</content>
+	</div>
+</body>
+</html>
