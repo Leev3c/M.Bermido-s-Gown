8c0969b9b83709ed7d17e5573af172ce4aae64be
Leev3c <82589504+Leev3c@users.noreply.github.com>
2021-04-29 21:24:43 +0800

    Added products.php
    Add the Database listed items here

---
+++ M.Bermidos Gown/products.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Products</title>
+	<link rel="stylesheet" href="css/products.css" />
+</head>
+<body>
+	<header>
+				
+			</div>
+		<div class="main-nav">
+				<ul>
+					<li>
+						<a href="index.html">Home</a>
+					</li>
+					<li>
+						<a href="products.html">Products</a>
+					</li>
+					<li>
+						<a href="services.html">Services</a>
+					</li>
+					<li>
+						<a href="aboutus.html">About us</a>
+					</li>
+					<li>
+						<a href="sign-in.html">Sign-in</a>
+					</li>
+				</ul>
+			</div>
+	</header>
+
+	<content>
+		
+	</content>
+		
+
+
+</body>
+</html>
