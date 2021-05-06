1fdaa588c4215c8b1394b381bb167563e25d3b0d
Leev3c <82589504+Leev3c@users.noreply.github.com>
2021-05-06 21:21:53 +0800

    Still missing a paragraph explaining services offered. OPEN TO ALL TO EDIT

---
+++ M.Bermidos Gown/services.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Services</title>
+	<link rel="stylesheet" href="css/services.css" />
+</head>
+<body>
+	<header>
+		
+				
+			</div>
+		<div class="main-nav">
+				<ul>
+					<li>
+						<a href="index.html">Home</a>
+					</li>
+					<li>
+						<a href="products.html">Products</a>
+					</li>
+					<li>
+						<a href="services.html">Services</a>
+					</li>
+					<li>
+						<a href="aboutus.html">About us</a>
+					</li>
+					<li>
+						<a href="sign-in.html">Sign-in</a>
+					</li>
+				</ul>
+		</div>
+	</header>
+
+	<content>
+		<div class="content">
+			<div class="services_intro">
+			<h1>Services offered</h1>
+				
+				<p>*FILL THIS SECTION WITH SERVICES*<em></em></p></div>
+
+		<!-- <table>
+			
+			<tr>
+				<td>
+
+					<h2>Razer Gold Pins</h2>
+					<p>Use Razer Gold in over 2,500 games and entertainment apps to purchase in-game items and unlock exclusive content.</p>
+					<ul>
+						<br>
+						<li>Gold Pins</li>
+						<li>MU origin Pins</li>
+						<li>World of Dragon Nest</li>
+						<li>And more!</li>
+					</ul>
+				</td>
+				<td>
+
+					<h2>Bills payment</h2>
+					<p>Skip long lines and pay bills anytime, anywhere with our services offered!</p>
+					<ul>
+						<br>
+						<li>Electric utilities</li>
+						<li>Water utilities</li>
+						<li>Cable/Internet</li>
+						<li>And more!</li>
+					</ul>
+				</td>
+				
+				<td>
+					<h2>Game Credits</h2>
+					<p>Certified online credit seller! Buy games and even in-game contents and more!</p>
+					<ul>
+						<br>
+						<li>Steam Wallet Credits</li>
+						<li>Garena Shells</li>
+						<li>Google Playstore Credits</li>
+						<li>And more!</li>
+					</ul>
+				</td>
+			</tr>
+
+		</table> -->
+</div>
+	</content>
+</body>
+</html>
