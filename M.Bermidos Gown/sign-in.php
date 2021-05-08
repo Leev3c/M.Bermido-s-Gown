1645ca0c63df9158b831dc3ed81b564a0e1b1b6d
Leev3c <82589504+Leev3c@users.noreply.github.com>
2021-05-08 17:47:09 +0800

    Sign up form

---
+++ M.Bermidos Gown/sign-in.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sign-in</title>
+	<link rel="stylesheet" type="text/css" href="css/sign-in.css">
+</head>
+
+	
+<body>
+	<div class="container">
+
+		<form class="box" action="sign-in.html" method="post">
+		<h1>Sign-in</h1>
+		<input type="text" name="Username" placeholder="Username">
+		<input type="password" name="Password" placeholder="Password">
+		<input type="submit" name="" value="Sign-in">
+		
+
+
+		</form>
+	</div>
+</body>
+</html>
