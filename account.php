9dba34b34cc368516ca2da9fa3ff917a74a2362b
julinoarevalo <77788878+julinoarevalo@users.noreply.github.com>
2021-05-13 21:38:35 -0700

    Add files via upload
    Adding of Login and Sign up Page with database

---
+++ account.php
@@ -0,0 +1,63 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Account</title>
+	<link rel="stylesheet" type="text/css" href="css/account.css">
+	
+</head>
+<body>
+	<div class="head">
+		<div class="container">
+			<div class="main-nav">
+				<div class="logo">
+					<img src="img/logo.png" width="175px">
+				</div>
+					<nav>
+						<ul>
+							<li><a href="index.php">Home</a></li>
+							<li><a href="products.php">Products</a></li>
+							<li><a href="services.php">Services</a></li>
+							<li><a href="aboutus.php">About Us</a></li>
+							<li><a href="" classACTIVE>Account</a></li>
+						</ul>
+					</nav>					
+
+				<img src="images/cart.png" width="30px" height="30px">
+
+			</div>	
+		</div>
+		
+	</div>
+	
+
+
+
+
+
+
+	<div class="footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-4">
+					<h3>Useful Links</h3>
+					<ul>
+						<li>Return Policy</li>
+						<li>Join Affiliate</li>
+					</ul>
+				</div>
+				<div class="col-4">
+					<h3>Follow Us</h3>
+					<ul>
+						<li>Facebook</li>
+						<li>Instagram</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
---
+++ bermidos.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 14, 2021 at 09:24 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bermidos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `age` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `mobilenumber` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `age`, `address`, `mobilenumber`, `email`, `password`) VALUES
+(1, 'Kenneth Zaragoza', '21', 'Tobog Oas, Albay', '097854123365', 'kennethzaragoza07@gmail.com', '12345'),
+(2, 'Julino Arevalo', '21', 'Maynaga Polangui, Albay', '09165189487', 'julinoxxx@gmail.com', '123456'),
+(3, 'Lovelyn Monda', '23', 'Balaba Polangui, Albay', '03789541236', 'lovelynmonda@gmail.com', '1234567');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
