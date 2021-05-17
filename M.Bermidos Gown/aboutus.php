bf4f2a1e804c88a0c9a5db2e9a8ec36827755d68
kinitaay <77792617+kinitaay@users.noreply.github.com>
2021-05-16 18:24:52 -0700

    Add files via upload

--- M.Bermidos Gown/aboutus.php
+++ M.Bermidos Gown/aboutus.php
@@ -2,8 +2,8 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Services</title>
-	<link rel="stylesheet" type="text/css" href="css/products.css">
+	<title>M.Bermido</title>
+	<link rel="stylesheet" type="text/css" href="css/index.css">
 </head>
 <body>
 	<div class="header">
@@ -14,10 +14,10 @@
 				</div>
 					<nav>
 						<ul>
-							<li><a href="index.php">Home</a></li>
+							<li><a href="index.php" class="active">Home</a></li>
 							<li><a href="products.php">Products</a></li>
 							<li><a href="services.php">Services</a></li>
-							<li><a href="aboutus.php" class="active">About Us</a></li>
+							<li><a href="aboutus.php">About Us</a></li>
 							<li><a href="login.php">Account</a></li>
 						</ul>
 					</nav>
@@ -25,9 +25,25 @@
 					<img src="images/cart.png" width="30px" height="30px">
 
 			</div>	
+
+			<div class="row">
+				<div class="col-2">		
+					<h1>M.Bermidos</h1>
+					<p>is a rental gown shop that offers Wedding Entourage Formal Wear, Wedding Gowns, Dresses(Evening, Prom, Pageant, Debut, Casual). Available also for Made to order gowns.</p><br>
+					<p align="center"><b>Main Branch Location:</b><br>Purok Tuba, Basud, Polangui, Albay, 4506 Philippines<br><br></p>
+					<p align="center"><b>Other Branch:</b><br>Windfield Subdivision, Alnay Polangui Albay, 4506 Philippines</p><br>
+					<p align="center"><b>For Inquiries:</b><br>Contact: 09475240622 / 09194566838 or message them in facebook and instagram  </p>
+					<!--<a href="products.php" class="btn">Rent Now &#8594 </a>--->
+				</div>
+				<div class="col-2">
+					<img src="img/col-2.png">
+					
+				</div>
+				
+			</div>
 		</div>
-		
 	</div>
+
 	<div class="footer">
 		<div class="container">
 			<div class="row">
