5aec360bd8d9688b7da4b9c2f6567f6ee5828e8a
Eilana3c <82512069+analiebalang@users.noreply.github.com>
2021-05-24 11:08:25 +0800

    Index added

---
+++ M.Bermido's Gown - Final -/database/website/index.php
@@ -0,0 +1,264 @@
+<?php
+include('config/dbconn.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png">
+    <link rel="icon" type="image/png" href="./assets/img/favicon.png">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title>M.Bermido's</title>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
+    <!-- CSS Files -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="assets/css/now-ui-kit.css?v=1.1.0" rel="stylesheet" />
+
+    <!--     inserted     -->
+    <link rel="stylesheet" href="plugins/datatables/dataTables.bootstrap.css">
+    <link href="assets/js/google-code-prettify/prettify.css" rel="stylesheet"/>
+    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet"/>
+    
+    <link href="assets/style.css" rel="stylesheet"/>
+    <!--     inserted     -->
+
+</head>
+
+<body class="index-page sidebar-collapse">
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg bg-light fixed-top navbar-transparent" color-on-scroll="50">
+        <div class="container">
+            <div class="navbar-translate">
+                <a href="" class="navbar-brand" rel="tooltip"data-placement="bottom" target="" style="color:#000000">
+                    M.Bermido's Gown
+                </a>
+                <button class="navbar-toggler navbar-toggler" type="button"  data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation" style="color:#000000">
+                    <span class="navbar-toggler-bar bar2"></span>
+                    <span class="navbar-toggler-bar bar3"></span>
+                    <span class="navbar-toggler-bar bar4"></span>
+                </button>
+            </div>
+            <div class="collapse navbar-collapse justify-content-end" id="navigation" >
+                <ul class="navbar-nav" >
+                    <li class="nav-item">
+                        <a href="index.php" class="nav-link" href="javascript:void(0)" onclick="scrollToDownload()">
+                            
+                            <p style="color:#000000">Home</p>
+                        </a>
+                    </li>
+					
+                    <li class="nav-item">
+                        <a class="nav-link" href="pages/products.php">
+                            <p style="color:#000000">Products</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="" onclick="scrollToDownload()">
+                            <p style="color:#000000">Cart</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="pages/user_login_page.php" class="nav-link" href="javascript:void(0)" onclick="scrollToDownload()">
+                            <p style="color:#000000">Login</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="pages/user_signup.php" class="nav-link" onclick="scrollToDownload()">
+                            <p style="color:#000000">Sign up</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" rel="tooltip" title="Like us on Facebook" data-placement="bottom" href="https://www.facebook.com/mbermido" target="_blank">
+                    <i class="fa fa-facebook-square" style="color:#000000"></i>
+                            <p style="color:#000000">Facebook</p>
+                        </a>
+                    </li>  
+                </ul>
+            </div>
+        </div>
+        </nav>
+
+    <!-- End Navbar -->
+    <div class="wrapper">
+        <div class="page-header clear-filter" filter-color="white">
+                <div class="container">
+                    <div class="content-center brand">
+                        <img src="./assets/img/bg.png"style="margin-top:15%; width: 600px; height: 600px">
+                        <h2 style="color:#000000">High Quality and Affordable Gowns for Rent!.</h2>
+                    </div>
+            </div>
+        </div>
+        <div class="main">
+            <div class="section section-basic">
+                <div class="container">
+                    <br>
+                    <div class="col-md-12">
+                        <h2 class="title">Products</h2>
+                        <div class="typography-line">
+                            <p>
+                            “The reason it seems that price is all your customers care about is that you haven’t given them anything else to care about.”-Seth Godin, American author, entrepreneur, marketer, and public speaker.
+                            </p>
+                        </div>
+                        <br>
+
+                        
+                        <center>
+                        <label><b>Search Product: &nbsp</b></label>       
+                                <form method="POST" action="index_search.php" >
+                                    <input type="image" title="Search" src="assets/img/search.png" alt="Search" />
+                                    <input type="text" name="search" class="search-query" placeholder="Enter product name">
+                                </form>
+                        </center>
+                    </div>
+                    <br><hr color="black">
+                </div>
+            </div>
+        </div>
+    </div>
+
+  <div class="tab-pane  active" id="">
+    <ul class="thumbnails">
+    <?php
+    $query = "SELECT * FROM products ORDER BY prod_name ASC";
+    $result = mysqli_query($dbconn,$query);
+    while($res = mysqli_fetch_array($result)) {  
+        $prod_id=$res['prod_id'];
+    
+?>   
+    <div class="row-sm-3">
+        <div class="thumbnail">
+            <?php if($res['prod_pic1'] != ""): ?>
+            <img src="uploads/<?php echo $res['prod_pic1']; ?>" width="300px" height="350px">
+            <?php else: ?>
+            <img src="uploads/default.png" width="300px" height="350px">
+            <?php endif; ?>
+        <div class="caption">
+          <h5><b><?php echo $res['prod_name'];?></b></h5>
+          <h6><a class="btn btn-dark btn-round" title="Click for more details!" href="pages/product_details.php?prod_id=<?php echo $res['prod_id'];?>"><i class="now-ui-icons gestures_tap-01"></i>View</a><medium class="pull-right">Php<?php echo $res['prod_price']; ?></medium></h6>
+        </div>
+
+        </div>
+      <hr color="black">
+      </div>
+             
+<?php }?> 
+
+      </ul>
+  </div>
+        
+
+
+    </div>     
+</div>
+    <div>
+
+        <footer class="footer" data-background-color="black">
+            <div class="container">
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="" target="_blank">
+                                INTERNET TECHNOLOGIES
+                            </a>
+                        </li>
+                        <li>
+                            IT Elective 3  
+                        </li>
+                    </ul>
+                </nav>
+                <div class="copyright">
+                    &copy;
+                    <script>
+                        document.write(new Date().getFullYear())
+                    </script>, BSIT-3C, Powerpuff Girls
+                </div>    
+            </div>
+        </footer>
+    </div>
+</body>
+<!--   Core JS Files   -->
+<script src="./assets/js/core/jquery.3.2.1.min.js" type="text/javascript"></script>
+<script src="./assets/js/core/popper.min.js" type="text/javascript"></script>
+<script src="./assets/js/core/bootstrap.min.js" type="text/javascript"></script>
+<!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
+<script src="./assets/js/plugins/bootstrap-switch.js"></script>
+<!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+<script src="./assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
+<!--  Plugin for the DatePicker, full documentation here: https://github.com/uxsolutions/bootstrap-datepicker -->
+<script src="./assets/js/plugins/bootstrap-datepicker.js" type="text/javascript"></script>
+<!-- Control Center for Now Ui Kit: parallax effects, scripts for the example pages etc -->
+<script src="./assets/js/now-ui-kit.js?v=1.1.0" type="text/javascript"></script>
+<script type="text/javascript">
+    $(document).ready(function() {
+        // the body of this function is in assets/js/now-ui-kit.js
+        nowuiKit.initSliders();
+    });
+
+    function scrollToDownload() {
+
+        if ($('.section-download').length != 0) {
+            $("html, body").animate({
+                scrollTop: $('.section-download').offset().top
+            }, 1000);
+        }
+    }
+</script>
+
+
+
+   <!---  inserted  -->
+    <script src="./plugins/datatables/jquery.dataTables.min.js"></script>
+    <script src="./plugins/datatables/dataTables.bootstrap.min.js"></script>
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/google-code-prettify/prettify.js"></script>
+    <script src="assets/js/application.js"></script>
+    <script src="assets/js/bootstrap-transition.js"></script>
+    <script src="assets/js/bootstrap-modal.js"></script>
+    <script src="assets/js/bootstrap-scrollspy.js"></script>
+    <script src="assets/js/bootstrap-alert.js"></script>
+    <script src="assets/js/bootstrap-dropdown.js"></script>
+    <script src="assets/js/bootstrap-tab.js"></script>
+    <script src="assets/js/bootstrap-tooltip.js"></script>
+    <script src="assets/js/bootstrap-popover.js"></script>
+    <script src="assets/js/bootstrap-button.js"></script>
+    <script src="assets/js/bootstrap-collapse.js"></script>
+    <script src="assets/js/bootstrap-carousel.js"></script>
+    <script src="assets/js/bootstrap-typeahead.js"></script>
+    <script src="assets/js/bootstrap-affix.js"></script>
+    <script src="assets/js/jquery.lightbox-0.5.js"></script>
+    <script src="assets/js/bootsshoptgl.js"></script>
+     <script type="text/javascript">
+    $(function() {
+        $('#gallery a').lightBox();
+    });
+    </script>
+
+    <!-- SlimScroll -->
+    <script src="./plugins/slimScroll/jquery.slimscroll.min.js"></script>
+    <!-- FastClick -->
+    <script src="./plugins/fastclick/fastclick.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="./plugins/app.min.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="./plugins/demo.js"></script>
+    <script src="./plugins/datatables/jquery.dataTables.min.js"></script>
+    <script src="./plugins/datatables/dataTables.bootstrap.min.js"></script>
+    <script>
+      $(function () {
+        $("#example1").DataTable({
+        });
+      });
+    </script>
+     <!--  inserted  -->
+
+</html>
